78005f4a7acd18e1fb59247e25cf5347351307d8
bayazidhassan <bayazidhassan776@gmail.com>
2025-11-29 04:22:27 +0600

    hide footer for about us section

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,13 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/footer/Footer";
 import NavBar from "../components/navbar/NavBar";
 
 const MainLayout = () => {
+  const { pathname } = useLocation();
+  // Route where footer should NOT appear
+  const hideFooterRoutes = ["/aboutUs"];
+
+  const shouldHideFooter = hideFooterRoutes.includes(pathname);
+
   return (
     <div className="pt-24">
       <NavBar></NavBar>
       <Outlet></Outlet>
-      <Footer></Footer>
+      {/* <Footer></Footer> */}
+      {/* do not show footer section when I will visit about us page */}
+      {!shouldHideFooter && <Footer></Footer>}
     </div>
   );
 };
