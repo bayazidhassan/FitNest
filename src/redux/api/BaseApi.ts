16a42df78231fbaa87671b95e46c98c5bde52279
bayazidhassan <bayazidhassan776@gmail.com>
2025-12-01 03:01:06 +0600

    change base url

--- src/redux/api/BaseApi.ts
+++ src/redux/api/BaseApi.ts
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/v1",
+    //baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://fit-nest-backend.vercel.app/",
     credentials: "include",
   }),
   endpoints: () => ({}),
