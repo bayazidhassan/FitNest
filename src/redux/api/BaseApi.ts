dc24daa8760f7d813728a74a48e5546f23557307
bayazidhassan <bayazidhassan776@gmail.com>
2025-12-01 03:23:46 +0600

    update base url

--- src/redux/api/BaseApi.ts
+++ src/redux/api/BaseApi.ts
@@ -4,7 +4,7 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     //baseUrl: "http://localhost:5000/api/v1",
-    baseUrl: "https://fit-nest-backend.vercel.app/",
+    baseUrl: "https://fit-nest-backend.vercel.app/api/v1",
     credentials: "include",
   }),
   endpoints: () => ({}),
