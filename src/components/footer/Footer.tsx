ce01aae2ff2d50bd0e27ef203426e1b455bb60db
bayazidhassan <bayazidhassan776@gmail.com>
2026-01-24 23:12:20 +0600

    UI improve:Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,6 @@ const Footer = () => {
   return (
     <footer className="bg-[#0F172A] text-white p-4 mt-20">
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
-        {/* Logo + Description */}
         <div className="flex flex-col justify-center space-y-1 md:space-y-4">
           <div className="flex items-center space-x-2">
             <img
@@ -18,28 +17,23 @@ const Footer = () => {
             Your fitness & e-commerce destination. Quality products to power your journey.
           </p>
         </div>
-
         <div className="md:flex justify-center space-y-4">
-          {/* Quick Links */}
           <div className="flex flex-col space-y-0 md:space-y-1">
             <h3 className="text-[#F97316] font-semibold mb-2">Quick Links</h3>
-            <Link to="/products" className="hover:text-[#0D9488]">
+            <Link to="/products" className="inline w-fit hover:text-[#0D9488]">
               Products
             </Link>
-            <Link to="/cart" className="hover:text-[#0D9488]">
+            <Link to="/cart" className="inline w-fit hover:text-[#0D9488]">
               Cart
             </Link>
-            <Link to="/aboutUs" className="hover:text-[#0D9488]">
+            <Link to="/aboutUs" className="inline w-fit hover:text-[#0D9488]">
               About Us
             </Link>
-            <Link to="/login" className="hover:text-[#0D9488]">
+            <Link to="/login" className="inline w-fit hover:text-[#0D9488]">
               Login
             </Link>
           </div>
-
           <div className="border-l border-gray-400 border-dashed mx-6 self-stretch"></div>
-
-          {/* Contact / Social */}
           <div className="flex flex-col space-y-1">
             <h3 className="text-[#F97316] font-semibold mb-2">Contact Us</h3>
             <p className="text-gray-300">Email: support@fitnest.com</p>
@@ -58,8 +52,6 @@ const Footer = () => {
           </div>
         </div>
       </div>
-
-      {/* Bottom copyright */}
       <div className="border-t border-gray-700 mt-4 md:mt-8 pt-4 text-center text-gray-400 text-sm">
         &copy; {new Date().getFullYear()} FitNest. All rights reserved.
       </div>
