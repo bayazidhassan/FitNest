af14f2917248801f3667ce36d2e4a2fe414a3695
bayazidhassan <bayazidhassan776@gmail.com>
2026-02-03 04:29:42 +0600

    menu bar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -278,7 +278,7 @@ const NavBar = () => {
               <DropdownMenuItem asChild>
                 <Link
                   to="/"
-                  className={`border border-white ${isActive('/') ? 'bg-slate-800 text-white font-bold' : 'text-white'}`}
+                  className={`border border-white ${isActive('/') ? 'bg-slate-800 text-orange-500 font-bold' : 'text-white'}`}
                 >
                   Home
                 </Link>
@@ -286,7 +286,7 @@ const NavBar = () => {
               <DropdownMenuItem asChild>
                 <Link
                   to="/products"
-                  className={`border border-white ${isActive('/products') ? 'bg-slate-800 text-white font-bold' : 'text-white'}`}
+                  className={`border border-white ${isActive('/products') ? 'bg-slate-800 text-orange-500 font-bold' : 'text-white'}`}
                 >
                   Products
                 </Link>
@@ -294,7 +294,7 @@ const NavBar = () => {
               <DropdownMenuItem asChild>
                 <Link
                   to="/aboutUs"
-                  className={`border border-white ${isActive('/aboutUs') ? 'bg-slate-800 text-white font-bold' : 'text-white'}`}
+                  className={`border border-white ${isActive('/aboutUs') ? 'bg-slate-800 text-orange-500 font-bold' : 'text-white'}`}
                 >
                   About Us
                 </Link>
@@ -303,7 +303,7 @@ const NavBar = () => {
                 <DropdownMenuItem asChild>
                   <Link
                     to={`/dashboard/${user.role}`}
-                    className={`border border-white ${isActive(`/dashboard/${user.role}`) ? 'bg-slate-800 text-white font-bold' : 'text-white'}`}
+                    className={`border border-white ${isActive(`/dashboard/${user.role}`) ? 'bg-slate-800 text-orange-500 font-bold' : 'text-white'}`}
                   >
                     Dashboard
                   </Link>
@@ -313,7 +313,7 @@ const NavBar = () => {
                 <DropdownMenuItem asChild>
                   <Link
                     to="/login"
-                    className={`border border-white ${isActive('/login') ? 'bg-slate-800 text-white font-bold' : 'text-white'}`}
+                    className={`border border-white ${isActive('/login') ? 'bg-slate-800 text-orange-500 font-bold' : 'text-white'}`}
                   >
                     Login
                   </Link>
