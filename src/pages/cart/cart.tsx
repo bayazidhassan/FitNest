a021eed0d882d74afa079ae55c17f9fa67bb0968
bayazidhassan <bayazidhassan776@gmail.com>
2026-03-06 16:15:22 +0600

    cart:improve ui

--- src/pages/cart/cart.tsx
+++ src/pages/cart/cart.tsx
@@ -69,11 +69,11 @@ const Cart = () => {
 
       <div className="flex flex-col md:flex-row gap-6">
         {/* LEFT SIDE – CART ITEMS */}
-        <div className="w-full md:w-2/3 flex flex-col gap-4">
+        <div className="w-full md:w-2/3 flex flex-col gap-4 max-h-[70vh] overflow-y-auto pr-2">
           {cartItems.map((product, index) => (
             <div
               key={product.product_id}
-              className="flex flex-col sm:flex-row justify-between items-start sm:items-center p-4 border border-gray-300 rounded-lg shadow-lg hover:shadow-md hover:bg-gray-100 transition"
+              className="flex flex-col sm:flex-row justify-between items-start sm:items-center p-4 border border-gray-300 bg-gray-50 rounded-lg shadow-md hover:shadow hover:bg-gray-100 transition"
             >
               {/* Product Info */}
               <div className="flex items-start sm:items-center gap-4 w-full sm:w-2/3">
@@ -136,7 +136,7 @@ const Cart = () => {
         </div>
 
         {/* RIGHT SIDE – SUMMARY */}
-        <div className="w-full md:w-1/3 p-6 border border-gray-300 rounded-lg shadow-lg bg-gray-50 h-fit md:sticky md:top-24">
+        <div className="w-full md:w-1/3 p-6 border border-gray-300 rounded-lg shadow-md bg-gray-50 h-fit">
           <h2 className="text-xl font-semibold text-gray-700 mb-4">Cart Summary</h2>
 
           <div className="flex justify-between text-gray-600 mb-2">
